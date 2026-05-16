82a926196cf6216d2d020c204e5b5d86b0394a2a
adamtbond <adam@eden-tech.co.uk>
2026-05-16 20:31:19 +0100

    First commit

--- src/main/java/com/edentech/firstserverapi/model/Note.java
+++ src/main/java/com/edentech/firstserverapi/model/Note.java
@@ -7,17 +7,33 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Note {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String text;
 
-    public Note() {}
+    public Note() {
+    }
 
     public Note(String text) {
         this.text = text;
     }
 
-    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
